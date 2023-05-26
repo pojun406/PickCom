270a08d89c54a47b885e856bce6f3e00822ebd4f
LeeWwwbbb <roozi8855@kyungmin.ac.kr>
2023-05-26 10:22:01 +0900

    백엔드
    0526(2)

--- pickCom/src/main/java/member/MemberDTO.java
+++ pickCom/src/main/java/member/MemberDTO.java
@@ -3,12 +3,14 @@ package member;
 public class MemberDTO {
     private String member_id;
     private String member_name;
-    private String nickName;
-    private String passwd;
-    private String email;
-    private String rank;
+    private String member_nickName;
+    private String member_passwd;
+    private String member_email;
+    private String member_rank;
     private String member_regDate;
+    private String member_lastDate;
     private boolean member_stat;
+    private int member_visitCount;
 
     public String getMember_id() {
         return member_id;
@@ -26,36 +28,36 @@ public class MemberDTO {
         this.member_name = member_name;
     }
 
-    public String getNickName() {
-        return nickName;
+    public String getMember_nickName() {
+        return member_nickName;
     }
 
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
+    public void setMember_nickName(String member_nickName) {
+        this.member_nickName = member_nickName;
     }
 
-    public String getPasswd() {
-        return passwd;
+    public String getMember_passwd() {
+        return member_passwd;
     }
 
-    public void setPasswd(String passwd) {
-        this.passwd = passwd;
+    public void setMember_passwd(String member_passwd) {
+        this.member_passwd = member_passwd;
     }
 
-    public String getEmail() {
-        return email;
+    public String getMember_email() {
+        return member_email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setMember_email(String member_email) {
+        this.member_email = member_email;
     }
 
-    public String getRank() {
-        return rank;
+    public String getMember_rank() {
+        return member_rank;
     }
 
-    public void setRank(String rank) {
-        this.rank = rank;
+    public void setMember_rank(String member_rank) {
+        this.member_rank = member_rank;
     }
 
     public String getMember_regDate() {
@@ -66,11 +68,27 @@ public class MemberDTO {
         this.member_regDate = member_regDate;
     }
 
+    public String getMember_lastDate() {
+        return member_lastDate;
+    }
+
+    public void setMember_lastDate(String member_lastDate) {
+        this.member_lastDate = member_lastDate;
+    }
+
     public boolean isMember_stat() {
         return member_stat;
     }
 
     public void setMember_stat(boolean member_stat) {
         this.member_stat = member_stat;
+    }
+
+    public int getMember_visitCount() {
+        return member_visitCount;
+    }
+
+    public void setMember_visitCount(int member_visitCount) {
+        this.member_visitCount = member_visitCount;
     }
 }
